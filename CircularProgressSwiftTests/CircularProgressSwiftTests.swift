97e5972c223bcf386e5cc55ed6ac173287ded6e7
Jamshed Alam <jamshed@faqinteractive.com.au>
2019-07-17 00:16:49 +0600

    Initial commit

---
+++ CircularProgressSwiftTests/CircularProgressSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  CircularProgressSwiftTests.swift
+//  CircularProgressSwiftTests
+//
+//  Created by MacBook Pro on 17/7/19.
+//  Copyright © 2019 Jamshed Alam. All rights reserved.
+//
+
+import XCTest
+@testable import CircularProgressSwift
+
+class CircularProgressSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
